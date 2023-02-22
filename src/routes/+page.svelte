6d7b3e837558f851d1541a346491b64a9e699ace
Luis Manzanero <manzanero.luis1995@gmail.com>
2023-02-23 01:36:21 +0800

    added meta tags for front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
+<svelte:head>
+  <!-- Primary Meta Tags -->
+  <title>Classroom Madness - Real time phonics, spelling, reading game</title>
+  <meta
+    name="title"
+    content="Classroom Madness - Real time phonics, spelling, reading game"
+  />
+  <meta
+    name="description"
+    content="A fun voice interactive game to practice phonics, spelling, and reading. Turn on your audio and start improving your English proficiency!"
+  />
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://metatags.io/" />
+  <meta
+    property="og:title"
+    content="Classroom Madness - Real time phonics, spelling, reading game"
+  />
+  <meta
+    property="og:description"
+    content="A fun voice interactive game to practice phonics, spelling, and reading. Turn on your audio and start improving your English proficiency!"
+  />
+  <meta
+    property="og:image"
+    content="https://classroom-madness.cyclic.app/classroom-madness.png"
+  />
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:url" content="https://metatags.io/" />
+  <meta
+    property="twitter:title"
+    content="Classroom Madness - Real time phonics, spelling, reading game"
+  />
+  <meta
+    property="twitter:description"
+    content="A fun voice interactive game to practice phonics, spelling, and reading. Turn on your audio and start improving your English proficiency!"
+  />
+  <meta
+    property="twitter:image"
+    content="https://classroom-madness.cyclic.app/classroom-madness.png"
+  />
+</svelte:head>
 <h1>Welcome to Classroom Madness!</h1>
 <a href="/room">Go back to room page</a>
