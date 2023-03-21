2e742ea039377789bedf61f71007ffa470d7d5d4
Luis Manzanero <manzanero.luis1995@gmail.com>
2023-03-21 14:15:49 +0800

    removed unused container styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,24 +9,6 @@
   import "../app.postcss";
 </script>
 
-<div class="container">
+<div>
   <slot />
 </div>
-
-<style>
-  /* .container {
-    width: 100vw;
-    height: 100vh;
-    background: rgb(236, 238, 174);
-    background: radial-gradient(
-      circle,
-      rgba(236, 238, 174, 1) 0%,
-      rgba(148, 233, 177, 1) 100%
-    );
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    color: rgb(35, 60, 60);
-  } */
-</style>
