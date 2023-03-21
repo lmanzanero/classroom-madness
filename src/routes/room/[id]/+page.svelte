a6ee78ef1ec312994c84ab54a3a0c9a930431944
Luis Manzanero <manzanero.luis1995@gmail.com>
2023-03-21 14:17:30 +0800

    added appbar with socket.io emitters

--- src/routes/room/[id]/+page.svelte
+++ src/routes/room/[id]/+page.svelte
@@ -1,12 +1,35 @@
 <script lang="ts">
   import type { PageData } from "./$types";
   import { onMount } from "svelte";
+  import { AppBar } from "@skeletonlabs/skeleton";
+
+  import { io } from "socket.io-client";
+
+  const socket = io("http://localhost:3000");
 
   export let data: PageData;
   let gameStart = false;
 
-  onMount(() => {});
+  onMount(() => {
+    document.cookie = "user_id=" + data.roomId;
+
+    socket.emit("joinRoom", { room: data.pageId, username: data.username });
+
+    socket.on("gameStatus", (status) => {
+      gameStart = status;
+    });
+    socket.on("message", (message) => {
+      console.log({ message });
+    });
+
+    socket.on("name", (name) => {
+      console.log({ name });
+    });
+  });
 </script>
 
-<h1>Hello room: {data.pageId} with: {data.username}</h1>
-<p>{gameStart}</p>
+<AppBar
+  ><strong>Room</strong>: {data.pageId} <strong>User</strong>: {data.username}
+  <strong>Game status:</strong>
+  {gameStart}</AppBar
+>
